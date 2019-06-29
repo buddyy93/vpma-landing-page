07bdb7c4941266c0ffdbfdde8cc28e18f3905084
buddyy93 <buddyy93@gmail.com>
2019-06-29 13:09:41 +0800

    delete our clients section

--- index.php
+++ index.php
@@ -373,7 +373,6 @@ Header
             </div>
 
         </div>
-
         <div class="container">
             <div class="row counters">
 
@@ -479,31 +478,6 @@ Header
 
         </div>
     </section><!-- #testimonials -->
-
-    <!--==========================
-      Clients Section
-    ============================-->
-    <section id="clients" class="wow fadeInUp">
-        <div class="container">
-
-            <header class="section-header">
-                <h3>Our Clients</h3>
-            </header>
-
-            <div class="owl-carousel clients-carousel">
-                <img src="img/clients/client-1.png" alt="">
-                <img src="img/clients/client-2.png" alt="">
-                <img src="img/clients/client-3.png" alt="">
-                <img src="img/clients/client-4.png" alt="">
-                <img src="img/clients/client-5.png" alt="">
-                <img src="img/clients/client-6.png" alt="">
-                <img src="img/clients/client-7.png" alt="">
-                <img src="img/clients/client-8.png" alt="">
-            </div>
-
-        </div>
-    </section><!-- #clients -->
-
 </main>
 
 <!--==========================

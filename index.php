634e2e7135fc818ab942c7a8cf48c20133252d12
buddyy93 <buddyy93@gmail.com>
2019-06-29 14:10:03 +0800

    another update

--- index.php
+++ index.php
@@ -484,7 +484,7 @@ Header
 
                     <div class="footer-links">
                         <div>
-                            <img class="mb-4" style="object-fit: cover" width="100%" height="150px"
+                            <img class="mb-4" style="" width="100%" height="150px"
                                  src="img/building.jpg" alt="">
                         </div>
                         <h4>Contact Us</h4>
